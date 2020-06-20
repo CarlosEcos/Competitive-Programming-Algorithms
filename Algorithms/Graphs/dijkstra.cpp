9d38e6f4a7ca8e64c489c8d3727cda905170eb24
CarlosEcos <60107356+CarlosEcos@users.noreply.github.com>
2020-06-19 22:26:38 -0400

    Create dijkstra.cpp

---
+++ Algorithms/Graphs/dijkstra.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+#define all(s) s.begin(),s.end()
+#define pb push_back
+#define INF 1e9
+using namespace std;
+
+vector <vector<pair<int,int>>> adj;
+vector <int> d;
+vector <bool> used;
+
+void dijkstra(int v){
+	int n = adj.size();
+	used.resize(n, false);
+	d.resize(n, INF);
+	priority_queue<pair<int,int>> pq;
+	d[v] = 0;
+	pq.push({0, v});
+	used[v] = true;
+	while(!pq.empty()){
+		int a = pq.top().second;
+		pq.pop();
+		if(used[a]) continue;
+		used[a] = true;
+		used[a] = false;
+		for(auto u : adj[a]){
+			if(d[a] + u.second < d[u.first]){
+				d[u.first] = d[a] + u.second;
+				pq.push({-d[u.first], u.first});
+			}
+		}
+	}
+}
+
+
+
